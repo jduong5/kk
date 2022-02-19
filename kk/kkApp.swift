1eb573ebad0e1d54e44c9bfb90f6bacf34181692
Jenny Duong <jennymimiduong@gmail.com>
2022-02-19 16:10:16 -0600

    Initial Commit

---
+++ kk/kkApp.swift
@@ -0,0 +1,17 @@
+//
+//  kkApp.swift
+//  kk
+//
+//  Created by Jenny Duong on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct kkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
